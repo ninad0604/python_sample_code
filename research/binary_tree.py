1fabdc6be78907aae6e01d14596b58c6571e1560
Ninad Loke <ninadloke123@gmail.com>
2019-07-29 22:36:14 -0700

    added binary tree

---
+++ research/binary_tree.py
@@ -0,0 +1,33 @@
+
+
+class Node:
+    def __init__(self,data):
+        self.data= data
+        self.left = None
+        self.right= None
+
+    def insert(self,data):
+        if data < self.data:
+            if self.left is None:
+                self.left = Node(data)
+            else:
+                self.left.insert(data)
+        elif data > self.data:
+            if self.right is None:
+                self.right = Node(data)
+            else:
+                self.right.insert(data)
+        else: self.data = data
+
+
+    def printTree(self):
+        if self.left:
+            self.left.printTree()
+        print(self.data)
+        if self.right:
+            self.right.printTree()
+    def __str__(self):
+        return f"Node({self.data})"
+
+    def __repr__(self):
+        return str({"data": [self.data],"left":[self.left],"right":[self.right]})
